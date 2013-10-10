4b5872ae090060880516064201fe4b6152e6060a
frozn <ag.github@exalounge.de>
2013-10-10 19:39:11 +0200

    - moved jquery lib - added core feature "create namespace"

---
+++ core/m.js
@@ -0,0 +1,24 @@
+/**
+ * create namespace
+ */
+var m = m || {};
+
+m.global = this;
+
+m.createNamespace = function(namespace) {
+    var root = m.global,
+        parts = namespace.split('.'),
+        i;
+
+    for (i = 0; i < parts.length; i++) {
+        var part = parts[i];
+
+        if (!root[part]) {
+            root[part] = {};
+        }
+
+        root = root[part];
+    }
+
+    return root;
+}
